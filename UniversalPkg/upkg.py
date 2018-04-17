6360b1c0f4aa28d9d8d3d61fb6c95bf78e6263cd
Matthew <Bean6754@users.noreply.github.com>
2018-04-17 13:32:43 +0100

    Update upkg.py

--- UniversalPkg/upkg.py
+++ UniversalPkg/upkg.py
@@ -1,11 +1,47 @@
+#!/usr/bin/python3
+
+import argparse
 import platform
 import system
 
-dist = platform.dist()
+# Check root.
+if os.geteuid() != 0:
+    exit("You need to have root privileges to run this script.\nExiting.")
 
-print(dist)
+# dist = platform.dist() - Depreciated since Python 2.6.
+dist = platform.linux_distribution()
+# jar = platform.java_ver()
+win = platform.win32_ver()
+mac = 
 
-# If statements.
-if "debian" in dist:
-  system(apt-get update && apt-get upgrade -y && apt-get dist-upgrade -y && apt-get full-upgrade -y)
-  print("Has")
+
+if "mac" in mac:
+  exit("This software only works with GNU/Linux.\nSorry.")
+if "win" in win:
+  exit("This software only works with GNU/Linux.\nSorry.")
+# print(dist)
+
+# Argparse.
+parser = argparse.ArgumentParser()
+parser.add_argument("-u", help="Update system.")
+args = parser.parse_args()
+if args.u:
+    print("Updating system.")
+    # If statements.
+    if "arch" in dist:
+      system(pacman -Syyu)
+    if "centos" in dist:
+      system(yum update -y)
+    if "fedora" in dist:
+      system(dnf update -y)
+      system(yum update -y) # For compatibility reasons. :(
+    if "debian" in dist:
+      system(apt-get update)
+      system(apt-get upgrade -y)
+      system(apt-get dist-upgrade -y)
+      system(apt-get full-upgrade -y)
+    if "Ubuntu" in dist:
+      system(apt-get update)
+      system(apt-get upgrade -y)
+      system(apt-get dist-upgrade -y)
+      system(apt-get full-upgrade -y)
