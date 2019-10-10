b527c5c11be8d9879bf13d19dffa1d7eefc2ea0e
Isabella Dean <Bean6754@users.noreply.github.com>
2019-10-10 16:27:53 +0100

    Update main.py

--- DivideAndConquer/main.py
+++ DivideAndConquer/main.py
@@ -1,3 +1,4 @@
+# Get user input for size of array then array values.
 num_array = list()
 num = input("Enter how many elements you want:")
 print ("Enter numbers in array: ")
@@ -5,20 +6,23 @@ for i in range(int(num)):
 	n = input("num :")
 	num_array.append(int(n))
 
+# Get array length (useful for if statement later).
 var_length = len(num_array)
 
+# Divide array by 2 if array size is greater than or equal to 2.
 if var_length >= 2:
-    # string.split()
     split_partA = num_array[:len(num_array)//2]
     split_partB = num_array[len(num_array)//2:]
     # print("Size is bigger than 4.\n")
     print(split_partA)
     print(split_partB)
 
+# Do nothing if array size is less than 2.
 elif var_length < 2:
-	# Do nothing.
 	print("Size is less than 2.\n")
 	print("No need to divide.\n")
+
+# If if statement fails for unknown reason. Exit the program.
 else:
     print("Error: An unknown error has occured.\n")
     exit()
