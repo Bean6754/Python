5fda1b3cdf56129991931710ab93e1f60bbf1ba6
Isabella Dean <Bean6754@users.noreply.github.com>
2019-10-10 16:25:12 +0100

    Update main.py

--- DivideAndConquer/main.py
+++ DivideAndConquer/main.py
@@ -7,10 +7,18 @@ for i in range(int(num)):
 
 var_length = len(num_array)
 
-if var_length > 4:
-	# string.split()
-	print("Size is bigger than 4.\n")
+if var_length >= 2:
+    # string.split()
+    split_partA = num_array[:len(num_array)//2]
+    split_partB = num_array[len(num_array)//2:]
+    # print("Size is bigger than 4.\n")
+    print(split_partA)
+    print(split_partB)
 
-elif var_length <= 4:
+elif var_length < 2:
 	# Do nothing.
-	print("Size is less than 4.\n")
+	print("Size is less than 2.\n")
+	print("No need to divide.\n")
+else:
+    print("Error: An unknown error has occured.\n")
+    exit()
